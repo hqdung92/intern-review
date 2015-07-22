c07cddba188780ecdd7f196c80859dd6bec6ac3f
FaceBook <hqdung92@gmail.com>
2015-07-22 09:18:41 +0700

    Event click button delete

--- app/src/main/java/intership/dev/contact/ContactsAdapter.java
+++ app/src/main/java/intership/dev/contact/ContactsAdapter.java
@@ -1,9 +1,12 @@
 package intership.dev.contact;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,24 +16,24 @@ import java.util.List;
 
 public class ContactsAdapter extends ArrayAdapter<Contacts> {
     Context mContext;
-    ArrayList<Contacts> mList = new ArrayList<Contacts>();
+    ArrayList<Contacts> mArrayList = new ArrayList<Contacts>();
 
     public ContactsAdapter(Context context, int resource, List<Contacts> objects) {
         super(context, resource, objects);
         // TODO Auto-generated constructor stub
 
         this.mContext = context;
-        this.mList = new ArrayList<Contacts>(objects);
+        this.mArrayList = new ArrayList<Contacts>(objects);
     }
 
     @Override
     public int getCount() {
-        return mList.size();
+        return mArrayList.size();
     }
 
     @Override
     public Contacts getItem(int position) {
-        return mList.get(position);
+        return mArrayList.get(position);
     }
 
     @Override
@@ -39,7 +42,7 @@ public class ContactsAdapter extends ArrayAdapter<Contacts> {
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, final ViewGroup parent) {
 
         final ViewHolder mViewHolder;
 
@@ -64,9 +67,40 @@ public class ContactsAdapter extends ArrayAdapter<Contacts> {
 
             mViewHolder = (ViewHolder) convertView.getTag();
         }
-        Contacts contact = mList.get(position);
+        final Contacts contact = mArrayList.get(position);
         mViewHolder.tvName.setText(contact.getmName());
         mViewHolder.imgAvata.setImageResource(contact.getmAvata());
+
+        mViewHolder.imgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog mDialog = new Dialog(mContext);
+                mDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                mDialog.setContentView(R.layout.dialog_delete);
+
+                TextView tvMessage = (TextView) mDialog.findViewById(R.id.tvMessage);
+                TextView tvOk = (TextView) mDialog.findViewById(R.id.tvOk);
+                TextView tvCancel = (TextView) mDialog.findViewById(R.id.tvCancel);
+
+                tvMessage.setText(Html.fromHtml("Are you sure you want to delete " + "<b>" + contact.getmName().toString() + "</b>" + "?"));
+
+                tvOk.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mArrayList.remove(position);
+                        notifyDataSetChanged();
+                        mDialog.cancel();
+                    }
+                });
+                tvCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mDialog.cancel();
+                    }
+                });
+                mDialog.show();
+            }
+        });
 
         return convertView;
     }
